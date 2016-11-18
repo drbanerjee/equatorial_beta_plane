1941aaf5c0136a7ffd5ff8bbd91a649be77ea83e
J.S. Oishi <jsoishi@gmail.com>
2016-11-18 08:16:01 -0500

    updated plot_timeseries

--- python/kelvin_wave_test/plot_timeseries.py
+++ python/kelvin_wave_test/plot_timeseries.py
@@ -1,8 +1,12 @@
+import os
+import sys
 import h5py
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-data =h5py.File("timeseries/timeseries_s1.h5","r")
+datadir = sys.argv[-1]
+
+data =h5py.File(os.path.join(datadir,"timeseries/timeseries_s1.h5"),"r")
 
 plt.plot(data['/scales/sim_time'][:],data['/tasks/urms'][:,0],linewidth=2,label='u')
 plt.plot(data['/scales/sim_time'][:],data['/tasks/vrms'][:,0],linewidth=2,label='v')
