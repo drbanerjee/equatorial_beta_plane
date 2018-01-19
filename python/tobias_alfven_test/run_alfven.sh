6e2d897c01d097a2d73cac39acf6162c138a8483
J. S. Oishi <jsoishi@gmail.com>
2018-01-18 22:16:19 -0500

    full path for dedalus on leavitt.

--- python/tobias_alfven_test/run_alfven.sh
+++ python/tobias_alfven_test/run_alfven.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #SBATCH --nodes=2
 
-source $HOME/build/dedalus_intel_mpi/bin/activate
+source /home/joishi/build/dedalus_intel_mpi/bin/activate
 
 date
 mpirun -np 32 python3 alfven_nonequatorial_beta_plane_A_omega.py --nx=1024 --ny=1024 --beta=0.
