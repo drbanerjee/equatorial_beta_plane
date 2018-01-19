a92f8c6b14527db2bd0d838820cd94c09a419b19
J.S. Oishi <jsoishi@gmail.com>
2018-01-18 20:44:12 -0500

    added very simple deterministic forcing

--- python/tobias_alfven_test/alfven_nonequatorial_beta_plane_A_omega.py
+++ python/tobias_alfven_test/alfven_nonequatorial_beta_plane_A_omega.py
@@ -3,17 +3,20 @@
 Run the driven non-equatorial beta plane equations from Tobias, Diamond, & Hughes (2007).
 
 Usage:
-    alfven_nonequatorial_beta_plane_A_omega.py [--Lx=<Lx> --Ly=<Ly> --nx=<nx> --ny=<ny> --B0=<B0> --beta=<beta> --eta=<eta> --nu=<nu> --dt=<dt>]
+    alfven_nonequatorial_beta_plane_A_omega.py [--Lx=<Lx> --Ly=<Ly> --nx=<nx> --ny=<ny> --mhd --B0=<B0> --beta=<beta> --eta=<eta> --nu=<nu> --dt=<dt> --G0=<G0> --n_modes=<n_modes>]
 
 Options:
-    --Lx=<Lx>                                length in latitude [default: 4]
-    --Ly=<Ly>                                length in longitude [default: 4]
+    --Lx=<Lx>                                length in latitude [default: 6.283185307179586]
+    --Ly=<Ly>                                length in longitude [default: 6.283185307179586]
     --nx=<nx>                                grid points in x [default: 128]
     --ny=<ny>                                grid points in y [default: 128]
     --B0=<B0>                                Zonal (x) background field strength [default: 0]
+    --mhd                                    Use MHD
     --beta=<beta>                            beta parameter [default: 5]
     --nu=<nu>                                viscosity [default: 1e-4]
     --eta=<eta>                              diffusivity [default: 1e-4]
+    --G0=<G0>                                forcing amplitude [default: 2.0]
+    --n_modes=<n_modes>                      number of modes to force with [default: 3]
 """
 import sys
 import os
@@ -35,14 +38,22 @@ ny = int(args['--ny'])
 beta = float(args['--beta'])
 nu = float(args['--nu'])
 B0 = float(args['--B0'])
+eta = float(args['--eta'])
+G0 = float(args['--G0'])
+n_modes = int(args['--n_modes'])
+
+if args['--mhd']:
+    mhd=True
+else:
+    mhd=False
 
 basename = sys.argv[0].split('.py')[0]
 
 data_dir = "scratch/" + basename
 data_dir += "_Lx{0:f}_Ly{1:f}_nx{2:d}_ny_{3:d}_beta{4:5.02e}_B0{5:5.02e}_nut{6:5.02e}".format(Lx,Ly,nx,ny,beta,B0,nu)
 
-if cheb:
-    data_dir += "_chebyshev"
+# if cheb:
+#     data_dir += "_chebyshev"
 
 config['logging']['filename'] = os.path.join(data_dir,'dedalus_log')
 config['logging']['file_level'] = 'DEBUG'
@@ -56,12 +67,15 @@ logger = logging.getLogger(__name__)
 # Create bases and domain
 # bases
 x_basis = de.Fourier('x', nx, interval=(0,Lx), dealias=3/2)
-y_basis = de.Fourier('y', ny, interval=(-Ly, Ly), dealias=3/2)
+y_basis = de.Fourier('y', ny, interval=(0, Ly), dealias=3/2)
 
 domain = de.Domain([x_basis,y_basis], grid_dtype=np.float64)
 
 # 2D Boussinesq magnetohydrodynamics on the beta plane
-bp = de.IVP(domain,variables=['omega','A','psi'])
+variables = ['omega','psi']
+if mhd:
+    variables.append('A')
+bp = de.IVP(domain,variables=variables)
 bp.parameters['beta'] = beta
 bp.parameters['B0'] = B0
 bp.parameters['Lx'] = Lx
@@ -69,12 +83,42 @@ bp.parameters['Ly'] = Ly
 bp.parameters['pi'] = np.pi
 bp.parameters['eta'] = eta
 bp.parameters['nu'] = nu
+bp.parameters['G0'] = G0
+bp.parameters['n_modes'] = n_modes
 
 bp.substitutions['vol_avg(A)']   = 'integ(A)/(Lx*Ly)'
 bp.substitutions['J(A,B)'] = 'dx(A)*dy(B) - dx(B)*dx(A)'
 bp.substitutions['Lap(A)'] = 'dx(dx(A)) + dy(dy(A))'
-bp.add_equation("dt(omega) + B0*Lap(dx(A)) - nu*Lap(omega) = J(psi,omega) + beta*dx(psi) + J(A,Lap(A))")
-bp.add_equation("dt(A) - B0*dx(psi) - eta*Lap(A) = J(psi, A)")
+bp.substitutions['u'] = 'dy(psi)'
+bp.substitutions['v'] = '-dx(psi)'
+
+def forcing(*args):
+    x = args[0].data
+    y = args[1].data
+    ampl = args[2].value 
+    n_modes = args[3].value
+
+    kx = [14, 12, 12]
+    ky = [0, 15, -15]
+    phase_x = [0, 0, 0]
+    phase_y = [0, 0, 0]
+
+    spatial_pattern = 0
+    for i in range(n_modes):
+        spatial_pattern += np.cos(kx[i]*x + phase_x[i])*np.cos(ky[i]*y + phase_y[i])/n_modes
+
+    return ampl*spatial_pattern
+
+def GF(*args, domain=domain, F=forcing):
+    return de.operators.GeneralFunction(domain, layout='g', func=F, args=args)
+
+de.operators.parseables['G'] = GF
+
+if mhd:
+    bp.add_equation("dt(omega) + B0*Lap(dx(A)) - nu*Lap(omega) = J(psi,omega) + beta*dx(psi) + J(A,Lap(A)) + G(x, y, G0, n_modes)")
+    bp.add_equation("dt(A) - B0*dx(psi) - eta*Lap(A) = J(psi, A)")
+else:
+    bp.add_equation("dt(omega) - nu*Lap(omega) = J(psi,omega) + beta*dx(psi) + G(x, y, G0, n_modes)")
 bp.add_equation("omega + Lap(psi) = 0", condition='nx != 0 or ny != 0')
 bp.add_equation("psi = 0", condition='nx == 0 and ny == 0')
 
@@ -82,19 +126,10 @@ bp.add_equation("psi = 0", condition='nx == 0 and ny == 0')
 dt = 0.01
 ts = de.timesteppers.RK443
 IVP = bp.build_solver(ts)
-IVP.stop_sim_time = 10.
+IVP.stop_sim_time = 100.
 IVP.stop_wall_time = np.inf
 IVP.stop_iteration = 10000000
 logger.info('Solver built')
-
-# Initial conditions
-x = domain.grid(0)
-y = domain.grid(1)
-eta = IVP.state['eta']
-u = IVP.state['u']
-
-# perturbations 
-omega
 
 if domain.distributor.rank == 0:
     if not os.path.exists('{:s}/'.format(data_dir)):
@@ -104,12 +139,17 @@ if domain.distributor.rank == 0:
 snapshots = IVP.evaluator.add_file_handler(os.path.join(data_dir,'snapshots'), sim_dt=1., max_writes=50)
 snapshots.add_system(IVP.state)
 
+slices = IVP.evaluator.add_file_handler(os.path.join(data_dir,'slices'), sim_dt=0.5, max_writes=50)
+slices.add_task("u",name="u")
+slices.add_task("v",name="v")
+
 timeseries = IVP.evaluator.add_file_handler(os.path.join(data_dir,'timeseries'),iter=10, max_writes=np.inf)
 timeseries.add_task("vol_avg(sqrt(u*u))",name='urms')
 timeseries.add_task("vol_avg(sqrt(v*v))",name='vrms')
-timeseries.add_task("vol_avg((v*v)/(u*u+v*v))",name='E_ky_ratio')
+timeseries.add_task("vol_avg(0.5*(u*u+v*v))",name='Ekin')
+timeseries.add_task("vol_avg((u*u)/(u*u+v*v))",name='Ekin_x_ratio')
 
-analysis_tasks = [snapshots, timeseries]
+analysis_tasks = [snapshots, slices, timeseries]
 
 # Main loop
 try:
